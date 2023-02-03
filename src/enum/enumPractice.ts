26293694288edb8efdc7f443a194a55f000a62eb
[아이엔소프트] 황호세 <hshwang@in-soft.co.kr>
2023-02-03 10:34:52 +0900

    chore: enum관련 설명 수정

--- src/enum/enumPractice.ts
+++ src/enum/enumPractice.ts
@@ -1,11 +1,12 @@
-//값이 헝가리안 표기법으로 되어있어서 
+//값이 헝가리안 표기법으로 되어있어서 직관적이지 못한 상태.
+//enum을 활용해서 직관성을 챙겨봤다.
 enum reName {
   사용자그룹명 = 'usr_grp_nm',
   상위사용자그룹설정 = 'up_usr_grp_nm',
   회원정보수정설정 = 'acct_mod_psb_yn',
   그룹공유폴더이름 = 'shar_str_nm',
   그룹공유폴더설정 = 'shar_str_usg_yn',
-}
+};
 
 
 const headerRename = (value: string) => {
@@ -20,8 +21,8 @@ const headerRename = (value: string) => {
       return value = reName.그룹공유폴더이름
     case '그룹공유폴더설정': 
       return value = reName.그룹공유폴더설정
-  }
+  };
   return '정의되지 않은 값입니다.';
-}
+};
 
 headerRename('그룹공유폴더설정');
